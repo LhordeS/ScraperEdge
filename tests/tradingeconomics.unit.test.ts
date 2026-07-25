c37f24946e6b87bbf425024eba3f5905f96507e4
Alain Mimeault <lhorde@msn.com>
2026-07-25 21:06:13 +0900

    Test for missing values or fields returning expected presets

--- tests/tradingeconomics.unit.test.ts
+++ tests/tradingeconomics.unit.test.ts
@@ -5,27 +5,49 @@ import { RawCalendarRow } from "../src/types/raw-calendar-row.js";
 describe("parseRow", () => {
   it("returns the proper object shape representative of an economic event", async () => {
     const rawRow: RawCalendarRow = {
-    time: "08:30",
-    country: "United States",
-    event: "Nonfarm Payrolls",
-    actual: "120K",
-    previous: "110K",
-    forecast: "115K",
-    importanceClass: "calendar-date-2",
-  };
+      time: "08:30",
+      country: "United States",
+      event: "Nonfarm Payrolls",
+      actual: "120K",
+      previous: "110K",
+      forecast: "115K",
+      importanceClass: "calendar-date-2",
+    };
 
     const event = parseRow(rawRow);
 
-    expect(event).toEqual(
-      {
-        time: "08:30",
-        country: "United States",
-        event: "Nonfarm Payrolls",
-        actual: "120K",
-        previous: "110K",
-        forecast: "115K",
-        importance: "Medium",
-      },
-    );
+    expect(event).toEqual({
+      time: "08:30",
+      country: "United States",
+      event: "Nonfarm Payrolls",
+      actual: "120K",
+      previous: "110K",
+      forecast: "115K",
+      importance: "Medium",
+    });
+  });
+
+  it("returns a null value for undefined items", async () => {
+    const rawRow = {
+      time: undefined,
+      country: undefined,
+      event: undefined,
+      actual: undefined,
+      previous: undefined,
+      forecast: undefined,
+      importance: undefined,
+    };
+
+    const event = parseRow(rawRow);
+
+    expect(event).toEqual({
+      time: "",
+      country: "",
+      event: "",
+      actual: null,
+      previous: null,
+      forecast: null,
+      importance: undefined,
+    });
   });
 });
